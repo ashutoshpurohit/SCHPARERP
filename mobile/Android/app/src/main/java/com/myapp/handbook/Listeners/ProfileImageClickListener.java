133f3048ac61087b94550b1eff7f10888e56d3c1
Sonal S Solanki <sonal.singh2288@gmail.com>
2017-12-02 13:17:22 +0530

    Image selection for profile photo

---
+++ mobile/Android/app/src/main/java/com/myapp/handbook/Listeners/ProfileImageClickListener.java
@@ -0,0 +1,10 @@
+package com.myapp.handbook.Listeners;
+
+/**
+ * Created by Ashutosh on 11/22/2017.
+ */
+
+public interface ProfileImageClickListener {
+
+    void onProfileImageClicked(String currentProfileId);
+}
