345d6c2ca4121a1ab07402566445aa7ddbe5b99b
Sonal S Solanki <sonal.singh2288@gmail.com>
2018-01-30 11:40:04 +0530

    Updated Database version

--- mobile/Android/app/src/main/java/com/myapp/handbook/data/HandBookDbHelper.java
+++ mobile/Android/app/src/main/java/com/myapp/handbook/data/HandBookDbHelper.java
@@ -36,7 +36,7 @@ import java.util.List;
 public class HandBookDbHelper extends SQLiteOpenHelper {
 
     static final String DATABASE_NAME = "handbook.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public HandBookDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
